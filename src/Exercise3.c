1ed42741614754eed2da64b3f040f61451754ae4
ITITIU20337 <ititiu20337@student.hcmiu.edu.vn>
2021-10-11 19:07:41 +0700

    ititiu20337

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -13,7 +13,7 @@ ______________________________________
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
-	int result=1;
+	
 	//Your codes here
     
 	while (testcase % 2 == 0) {
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
 	 printf("*");
 	 testcase = testcase / 2;}
      
-	for(int i=3;i<= sqrt(testcase); i= i + 2)
+	for(int i=3;i<= testcase; i= i + 2)
 	{
 	 while (testcase % i == 0 ){
 	 printf("%d ", i) ;
